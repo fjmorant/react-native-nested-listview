2ba9f23ca0f943ce31cc3cbefb2b3c65bdd84b75
Javier <javier.morant84@gmail.com>
2019-08-24 19:54:02 +0100

    Converted NestedRow in a functional component

--- src/NestedRow.tsx
+++ src/NestedRow.tsx
@@ -1,39 +1,44 @@
 /* @flow */
 
-import * as React from 'react'
-import {StyleSheet, View} from 'react-native'
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
 
 const styles = StyleSheet.create({
   nestedRow: {
     flex: 1,
-    justifyContent: 'center',
-  },
-})
+    justifyContent: "center"
+  }
+});
 
 export interface IProps {
-  height?: number
-  children: any
-  level?: number
-  paddingLeftIncrement?: number,
-  style?: any
+  height?: number;
+  children: any;
+  level?: number;
+  paddingLeftIncrement?: number;
+  style?: any;
 }
 
-export default class NestedRow extends React.PureComponent<IProps> {
-  public render() {
-    const {height = 50, children, level = 0, paddingLeftIncrement = 10, style} = this.props
+const NestedRow = ({
+  height = 50,
+  children,
+  level = 0,
+  paddingLeftIncrement = 10,
+  style
+}: IProps) => {
+  return (
+    <View
+      style={[
+        styles.nestedRow,
+        {
+          ...style,
+          height,
+          paddingLeft: level * paddingLeftIncrement
+        }
+      ]}
+    >
+      {children}
+    </View>
+  );
+};
 
-    return (
-      <View
-        style={[
-          styles.nestedRow,
-          {
-            ...style,
-            height,
-            paddingLeft: level * paddingLeftIncrement,
-          },
-        ]}>
-        {children}
-      </View>
-    )
-  }
-}
+export default NestedRow;
