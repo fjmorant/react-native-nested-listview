dae755d2fe51fd248cc02b3f3cc41e40beec8c9f
Javier <javier.morant84@gmail.com>
2019-08-25 10:02:22 +0100

    Converted to React Hooks

--- src/NestedListView.tsx
+++ src/NestedListView.tsx
@@ -1,133 +1,136 @@
 /* @flow */
 
-import isEqual from 'lodash.isequal'
-import * as React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
-import * as shortid from 'shortid'
-import NodeView, {INode} from './NodeView'
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as shortid from "shortid";
+import NodeView, { INode } from "./NodeView";
 
 const styles = StyleSheet.create({
   errorContainer: {
-    borderColor: 'rgb(84, 85, 86)',
-    backgroundColor: 'rgb(237, 57, 40)',
+    borderColor: "rgb(84, 85, 86)",
+    backgroundColor: "rgb(237, 57, 40)",
     borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60
   },
   errorText: {
-    color: 'rgb(255, 255, 255)',
+    color: "rgb(255, 255, 255)",
     fontSize: 17,
-    fontWeight: 'bold',
-  },
-})
+    fontWeight: "bold"
+  }
+});
 
 export interface IProps {
-  data: any
-  extraData?: any
-  renderNode: (elem: any, level?: number) => any
-  onNodePressed?: () => any
-  getChildrenName: (elem: any) => any
-  style?: any
+  data: any;
+  extraData?: any;
+  renderNode: (elem: any, level?: number) => any;
+  onNodePressed?: () => any;
+  getChildrenName: (elem: INode) => any;
+  style?: any;
 }
 
 export interface IState {
-  root: any
+  root: any;
 }
 
-export default class NestedListView extends React.PureComponent<
-  IProps,
-  IState
-> {
-  public componentWillMount() {
-    this.setState({root: this.generateRootNode(this.props)})
-  }
-
-  public componentWillReceiveProps(nextProps: any) {
-    if (!isEqual(this.props.data, nextProps.data)) {
-      this.setState({root: this.generateRootNode(nextProps)})
-    }
-  }
-
-  public generateIds = (node?: INode) => {
+const NestedListView = ({
+  getChildrenName,
+  renderNode,
+  data,
+  onNodePressed,
+  extraData
+}: IProps) => {
+  const generateIds = (node?: INode) => {
     if (!node) {
-      return
+      return;
     }
 
-    const childrenName: string = this.props.getChildrenName(node) || 'items'
-    let children = node[childrenName]
+    const childrenName: string = getChildrenName(node) || "items";
+    let children = node[childrenName];
 
     if (children) {
       if (!Array.isArray(children)) {
-        children = Object.keys(children).map((key: string) => children[key])
+        children = Object.keys(children).map((key: string) => children[key]);
       }
       node[childrenName] = children.map((_: INode, index: number) =>
-        this.generateIds(children[index])
-      )
+        generateIds(children[index])
+      );
     }
 
-    node._internalId = shortid.generate()
+    node._internalId = shortid.generate();
 
-    return node
-  }
+    return node;
+  };
 
-  public getChildrenName = (node: INode) => {
-    if (node.name === 'root') {
-      return 'items'
-    }
-
-    return this.props.getChildrenName
-      ? this.props.getChildrenName(node)
-      : 'items'
-  }
-
-  public renderErrorMessage(prop: string) {
-    return (
-      <View style={styles.errorContainer}>
-        <Text style={styles.errorText}>prop {prop} has not been passed</Text>
-      </View>
-    )
-  }
-
-  public render() {
-    const {data, getChildrenName, onNodePressed, renderNode} = this.props
-
-    if (!getChildrenName) {
-      return this.renderErrorMessage('getChildrenName')
-    }
-
-    if (!renderNode) {
-      return this.renderErrorMessage('renderNode')
-    }
-
-    if (!data) {
-      return this.renderErrorMessage('data')
-    }
-
-    return (
-      <NodeView
-        getChildrenName={this.getChildrenName}
-        node={this.state.root}
-        onNodePressed={onNodePressed}
-        generateIds={this.generateIds}
-        level={0}
-        renderNode={renderNode}
-        extraData={this.props.extraData}
-      />
-    )
-  }
-
-  private generateRootNode = (props: any): INode => {
+  const generateRootNode = (props: IProps): INode => {
     return {
       _internalId: shortid.generate(),
       items: props.data
         ? props.data.map((_: INode, index: number) =>
-            this.generateIds(props.data[index])
+            generateIds(props.data[index])
           )
         : [],
-      name: 'root',
+      name: "root",
       opened: true,
-      hidden: true,
+      hidden: true
+    };
+  };
+  const [_root, setRoot]: [INode, (_root: INode) => void] = useState(
+    generateRootNode({ getChildrenName, renderNode, data, onNodePressed })
+  );
+
+  useEffect(() => {
+    setRoot(
+      generateRootNode({
+        getChildrenName,
+        renderNode,
+        data,
+        onNodePressed,
+        extraData
+      })
+    );
+  }, [data, extraData, getChildrenName, renderNode, onNodePressed]);
+
+  const _getChildrenName = (node: INode) => {
+    if (node.name === "root") {
+      return "items";
     }
+
+    return getChildrenName ? getChildrenName(node) : "items";
+  };
+
+  const renderErrorMessage = (prop: string) => {
+    return (
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorText}>prop {prop} has not been passed</Text>
+      </View>
+    );
+  };
+
+  if (!getChildrenName) {
+    return renderErrorMessage("getChildrenName");
   }
-}
+
+  if (!renderNode) {
+    return renderErrorMessage("renderNode");
+  }
+
+  if (!data) {
+    return renderErrorMessage("data");
+  }
+
+  return (
+    <NodeView
+      getChildrenName={_getChildrenName}
+      node={_root}
+      onNodePressed={onNodePressed}
+      generateIds={generateIds}
+      level={0}
+      renderNode={renderNode}
+      extraData={extraData}
+    />
+  );
+};
+
+export default NestedListView;
