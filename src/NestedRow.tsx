c1814e53ba3686207236358d82c7e9ce171f3d5b
Francisco Javier Morant Moya <javier.morant84@gmail.com>
2019-11-17 19:11:51 +0000

    Prettier config

--- src/NestedRow.tsx
+++ src/NestedRow.tsx
@@ -5,36 +5,42 @@ import isEqual from 'react-fast-compare'
 import {StyleSheet, View} from 'react-native'
 
 const styles = StyleSheet.create({
-  nestedRow: {
-    flex: 1,
-    justifyContent: 'center',
-  },
+    nestedRow: {
+        flex: 1,
+        justifyContent: 'center',
+    },
 })
 
 export interface IProps {
-  height?: number
-  children: any
-  level?: number
-  paddingLeftIncrement?: number
-  style?: any
+    height?: number
+    children: any
+    level?: number
+    paddingLeftIncrement?: number
+    style?: any
 }
 
 const NestedRow = React.memo(
-  ({height, children, level = 0, paddingLeftIncrement = 10, style}: IProps) => (
-    <View
-      style={[
-        styles.nestedRow,
-        {
-          ...style,
-          paddingLeft: level * paddingLeftIncrement,
-        },
-        height ? {height} : {},
-      ]}
-    >
-      {children}
-    </View>
-  ),
-  isEqual
+    ({
+        height,
+        children,
+        level = 0,
+        paddingLeftIncrement = 10,
+        style,
+    }: IProps) => (
+        <View
+            style={[
+                styles.nestedRow,
+                {
+                    ...style,
+                    paddingLeft: level * paddingLeftIncrement,
+                },
+                height ? {height} : {},
+            ]}
+        >
+            {children}
+        </View>
+    ),
+    isEqual
 )
 
 export default NestedRow
