e0ac7f40098b7f1338eac10bb0ef1d6d247ac278
Francisco Javier Morant Moya <javier.morant84@gmail.com>
2019-11-17 18:49:00 +0000

    Dynamic height if necessary

--- src/NestedRow.tsx
+++ src/NestedRow.tsx
@@ -5,42 +5,36 @@ import isEqual from 'react-fast-compare'
 import {StyleSheet, View} from 'react-native'
 
 const styles = StyleSheet.create({
-    nestedRow: {
-        flex: 1,
-        justifyContent: 'center',
-    },
+  nestedRow: {
+    flex: 1,
+    justifyContent: 'center',
+  },
 })
 
 export interface IProps {
-    height?: number
-    children: any
-    level?: number
-    paddingLeftIncrement?: number
-    style?: any
+  height?: number
+  children: any
+  level?: number
+  paddingLeftIncrement?: number
+  style?: any
 }
 
 const NestedRow = React.memo(
-    ({
-        height = 50,
-        children,
-        level = 0,
-        paddingLeftIncrement = 10,
-        style,
-    }: IProps) => (
-        <View
-            style={[
-                styles.nestedRow,
-                {
-                    ...style,
-                    height,
-                    paddingLeft: level * paddingLeftIncrement,
-                },
-            ]}
-        >
-            {children}
-        </View>
-    ),
-    isEqual
+  ({height, children, level = 0, paddingLeftIncrement = 10, style}: IProps) => (
+    <View
+      style={[
+        styles.nestedRow,
+        {
+          ...style,
+          paddingLeft: level * paddingLeftIncrement,
+        },
+        height ? {height} : {},
+      ]}
+    >
+      {children}
+    </View>
+  ),
+  isEqual
 )
 
 export default NestedRow
