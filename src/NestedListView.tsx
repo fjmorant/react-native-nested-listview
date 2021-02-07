8087879222920a915c6d6378b2d100955ee0155c
fjmorant <javier.morant84@gmail.com>
2021-02-07 23:24:35 +0000

    Converted all with latest deps

--- src/NestedListView.tsx
+++ src/NestedListView.tsx
@@ -1,156 +1,170 @@
-import hashObjectGenerator from 'object-hash'
-import React, {useEffect, useState} from 'react'
-import isEqual from 'react-fast-compare'
-import {StyleSheet, Text, View} from 'react-native'
-import shortid from 'shortid'
-import NodeView, {INode} from './NodeView'
+import hashObjectGenerator from 'object-hash';
+import React, { useCallback, useEffect, useState } from 'react';
+import isEqual from 'react-fast-compare';
+import { StyleSheet, Text, View } from 'react-native';
+import shortid from 'shortid';
+import { INode, NodeView } from './NodeView';
 
 const styles = StyleSheet.create({
-    errorContainer: {
-        borderColor: 'rgb(84, 85, 86)',
-        backgroundColor: 'rgb(237, 57, 40)',
-        borderWidth: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 60,
-    },
-    errorText: {
-        color: 'rgb(255, 255, 255)',
-        fontSize: 17,
-        fontWeight: 'bold',
-    },
-})
+  errorContainer: {
+    borderColor: 'rgb(84, 85, 86)',
+    backgroundColor: 'rgb(237, 57, 40)',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+  },
+  errorText: {
+    color: 'rgb(255, 255, 255)',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
 
 export interface IProps {
-    data: any
-    extraData?: any
-    renderNode: (elem: INode, level?: number) => any
-    onNodePressed?: (node?: INode) => void
-    getChildrenName: (elem: any) => any
-    style?: StyleSheet
-    keepOpenedState?: boolean
+  data: any;
+  extraData?: any;
+  renderNode: (elem: INode, level?: number) => any;
+  onNodePressed?: (node?: INode) => void;
+  getChildrenName?: (elem: any) => any;
+  style?: StyleSheet;
+  keepOpenedState?: boolean;
 }
 
 export interface IState {
-    root: INode
+  root: INode;
 }
 
 const NestedListView = React.memo(
-    ({
+  ({
+    getChildrenName,
+    renderNode,
+    data,
+    onNodePressed,
+    extraData,
+    keepOpenedState,
+  }: IProps) => {
+    const generateIds = useCallback(
+      (node?: INode) => {
+        if (!node) {
+          return {
+            _internalId: shortid.generate(),
+          };
+        }
+
+        const childrenName: string = getChildrenName
+          ? getChildrenName(node)
+          : 'items';
+        let children = node[childrenName];
+
+        if (children) {
+          if (!Array.isArray(children)) {
+            children = Object.keys(children).map(
+              (key: string) => children[key],
+            );
+          }
+          node[childrenName] = children.map((_: INode, index: number) =>
+            generateIds(children[index]),
+          );
+        }
+        if (node._internalId) {
+          // @ts-ignore
+          delete node._internalId;
+        }
+
+        node._internalId = hashObjectGenerator(node, {
+          algorithm: 'md5',
+          encoding: 'base64',
+        });
+
+        return node;
+      },
+      [getChildrenName],
+    );
+
+    const generateRootNode = useCallback(
+      (props: IProps): INode => {
+        return {
+          _internalId: 'root',
+          items: props.data
+            ? props.data.map((_: INode, index: number) =>
+                generateIds(props.data[index]),
+              )
+            : [],
+          name: 'root',
+          opened: true,
+          hidden: true,
+        };
+      },
+      [generateIds],
+    );
+
+    const [_root, setRoot]: [INode, (_root: INode) => void] = useState(
+      generateRootNode({
         getChildrenName,
         renderNode,
         data,
         onNodePressed,
-        extraData,
-        keepOpenedState,
-    }: IProps) => {
-        const generateIds = (node?: INode) => {
-            if (!node) {
-                return {
-                    _internalId: shortid.generate(),
-                }
-            }
+      }),
+    );
 
-            const childrenName: string = getChildrenName(node) || 'items'
-            let children = node[childrenName]
+    useEffect(() => {
+      setRoot(
+        generateRootNode({
+          getChildrenName,
+          renderNode,
+          data,
+          onNodePressed,
+          extraData,
+        }),
+      );
+    }, [
+      data,
+      extraData,
+      getChildrenName,
+      renderNode,
+      onNodePressed,
+      generateRootNode,
+    ]);
 
-            if (children) {
-                if (!Array.isArray(children)) {
-                    children = Object.keys(children).map(
-                        (key: string) => children[key]
-                    )
-                }
-                node[childrenName] = children.map((_: INode, index: number) =>
-                    generateIds(children[index])
-                )
-            }
-            if (node._internalId) {
-                delete node._internalId
-            }
+    const _getChildrenName = (node: INode) => {
+      if (node.name === 'root') {
+        return 'items';
+      }
 
-            node._internalId = hashObjectGenerator(node, {
-                algorithm: 'md5',
-                encoding: 'base64',
-            })
+      return getChildrenName ? getChildrenName(node) : 'items';
+    };
 
-            return node
-        }
+    const renderErrorMessage = (prop: string) => {
+      return (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>prop {prop} has not been passed</Text>
+        </View>
+      );
+    };
 
-        const generateRootNode = (props: IProps): INode => {
-            return {
-                _internalId: 'root',
-                items: props.data
-                    ? props.data.map((_: INode, index: number) =>
-                          generateIds(props.data[index])
-                      )
-                    : [],
-                name: 'root',
-                opened: true,
-                hidden: true,
-            }
-        }
-        // tslint:disable-next-line:variable-name
-        const [_root, setRoot]: [INode, (_root: INode) => void] = useState(
-            generateRootNode({
-                getChildrenName,
-                renderNode,
-                data,
-                onNodePressed,
-            })
-        )
+    if (!renderNode) {
+      return renderErrorMessage('renderNode');
+    }
 
-        useEffect(() => {
-            setRoot(
-                generateRootNode({
-                    getChildrenName,
-                    renderNode,
-                    data,
-                    onNodePressed,
-                    extraData,
-                })
-            )
-        }, [data, extraData, getChildrenName, renderNode, onNodePressed])
-        // tslint:disable-next-line:variable-name
-        const _getChildrenName = (node: INode) => {
-            if (node.name === 'root') {
-                return 'items'
-            }
+    if (!data) {
+      return renderErrorMessage('data');
+    }
 
-            return getChildrenName ? getChildrenName(node) : 'items'
-        }
+    return (
+      <NodeView
+        getChildrenName={_getChildrenName}
+        node={_root}
+        onNodePressed={onNodePressed}
+        generateIds={generateIds}
+        level={0}
+        renderNode={renderNode}
+        extraData={extraData}
+        keepOpenedState={keepOpenedState}
+      />
+    );
+  },
+  isEqual,
+);
 
-        const renderErrorMessage = (prop: string) => {
-            return (
-                <View style={styles.errorContainer}>
-                    <Text style={styles.errorText}>
-                        prop {prop} has not been passed
-                    </Text>
-                </View>
-            )
-        }
-
-        if (!renderNode) {
-            return renderErrorMessage('renderNode')
-        }
-
-        if (!data) {
-            return renderErrorMessage('data')
-        }
-
-        return (
-            <NodeView
-                getChildrenName={_getChildrenName}
-                node={_root}
-                onNodePressed={onNodePressed}
-                generateIds={generateIds}
-                level={0}
-                renderNode={renderNode}
-                extraData={extraData}
-                keepOpenedState={keepOpenedState}
-            />
-        )
-    },
-    isEqual
-)
-
-export default NestedListView
+export { NestedListView };
--- src/NestedRow.test.tsx
+++ src/NestedRow.test.tsx
@@ -1,48 +1,49 @@
-import * as React from 'react'
-import {Text} from 'react-native'
-// tslint:disable-next-line:no-implicit-dependencies
-import * as renderer from 'react-test-renderer'
-import NestedRow from './NestedRow'
+import * as React from 'react';
+import { Text } from 'react-native';
+import { NestedRow } from './NestedRow';
+import { render, waitFor } from '@testing-library/react-native';
 
 describe('NestedListView', () => {
-    test('renders with simple array', () => {
-        const nestedListView = renderer
-            .create(
-                <NestedRow
-                    level={1}
-                    style={{borderColor: 'black', borderWidth: 1}}
-                >
-                    <Text>Test</Text>
-                </NestedRow>
-            )
-            .toJSON()
-        expect(nestedListView).toMatchSnapshot()
-    })
+  test('renders with succesfully', async () => {
+    const { UNSAFE_getByType } = render(
+      <NestedRow level={1} style={{ borderColor: 'black', borderWidth: 1 }}>
+        <Text>Test</Text>
+      </NestedRow>,
+    );
 
-    test('renders with simple array without level', () => {
-        const nestedListView = renderer
-            .create(
-                <NestedRow style={{borderColor: 'black', borderWidth: 1}}>
-                    <Text>Test</Text>
-                </NestedRow>
-            )
-            .toJSON()
-        expect(nestedListView).toMatchSnapshot()
-    })
+    await waitFor(() => {
+      const component = UNSAFE_getByType(NestedRow);
+      expect(component).toBeDefined();
+    });
+  });
 
-    test('renders customised', () => {
-        const nestedListView = renderer
-            .create(
-                <NestedRow
-                    level={1}
-                    height={60}
-                    paddingLeftIncrement={15}
-                    style={{borderColor: 'black', borderWidth: 1}}
-                >
-                    <Text>Test</Text>
-                </NestedRow>
-            )
-            .toJSON()
-        expect(nestedListView).toMatchSnapshot()
-    })
-})
+  test('renders without level passed', async () => {
+    const { UNSAFE_getByType } = render(
+      <NestedRow style={{ borderColor: 'black', borderWidth: 1 }}>
+        <Text>Test</Text>
+      </NestedRow>,
+    );
+
+    await waitFor(() => {
+      const component = UNSAFE_getByType(NestedRow);
+      expect(component).toBeDefined();
+    });
+  });
+
+  test('renders with height passed', async () => {
+    const { UNSAFE_getByType } = render(
+      <NestedRow
+        level={1}
+        height={60}
+        paddingLeftIncrement={15}
+        style={{ borderColor: 'black', borderWidth: 1 }}>
+        <Text>Test</Text>
+      </NestedRow>,
+    );
+
+    await waitFor(() => {
+      const component = UNSAFE_getByType(NestedRow);
+      expect(component).toBeDefined();
+    });
+  });
+});
