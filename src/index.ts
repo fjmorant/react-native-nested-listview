a0b3251464d6ec424d343e9f039293e0588935f5
fjmorant <javier.morant84@gmail.com>
2021-02-13 18:53:54 +0000

    Export INode

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { NestedListView } from './NestedListView';
 import { NestedRow } from './NestedRow';
+import { INode } from './NodeView';
 
 export { NestedRow };
+
+export { INode };
 
 export default NestedListView;
